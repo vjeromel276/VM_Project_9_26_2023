199e379fb5e4ffbe62f436d382536bf7ac011137
vjeromel276 <49462377+vjeromel276@users.noreply.github.com>
2024-01-16 13:00:27 -0500

    figuring it out

--- force-app/main/default/lwc/visionMetrixApp/visionMetrixApp.js
+++ force-app/main/default/lwc/visionMetrixApp/visionMetrixApp.js
@@ -1,3 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import getParentCases from '@salesforce/apex/VisionMetrixAppController.getParentCases';
+import getChildCases from '@salesforce/apex/VisionMetrixAppController.getChildCases';
 
-export default class VisionMetrixApp extends LightningElement {}
+export default class VisionMetrixApp extends LightningElement {
+
+    parentCases = [];
+
+    connectedCallback () {
+        this.getParentCases();
+    }
+
+    getParentCases () {
+        getParentCases()
+            .then(result => {
+                this.parentCases = result;
+                this.parentCases.forEach( parentCase => {  
+                    console.log( 'parentCase: ' + JSON.stringify( parentCase ));
+                    let childCases = [];
+                    getChildCases( { parentCaseId: parentCase.Id } )
+                        .then( result => {
+                            childCases = result;
+                            console.log( 'childCases: ' + JSON.stringify( childCases ) );
+                            parentCase.childCases = childCases;
+                            console.log( 'parentCase: ' + JSON.stringify( parentCase ));
+                        } )
+                        .catch( error => {
+                            this.error = error;
+                        } );
+                    
+                });
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    }
+
+}
